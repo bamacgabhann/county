903b83ca384f589183ff36875a20ca85c60da48a
bamacgabhann <127785144+breandan.macgabhann@users.noreply.github.com>
2025-04-05 19:14:38 +0100

    work in progress

--- src/county/create_schema.py
+++ src/county/create_schema.py
@@ -10,35 +10,113 @@ from sqlalchemy import (
     create_engine,
 )
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 Base = declarative_base()
 
 
 class Club(Base):
     __tablename__ = "clubs"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
-    location = Column(Geometry("POINT"), nullable=False)  # Geospatial data
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    fullname: Mapped[str] = mapped_column(String, nullable=False)
+    ainm = Column(String)
+    location = Column(Geometry("POINT"))  # Geospatial data
+
+    def __init__(self, name, fullname, ainm=None, location=None):
+        self.name = name
+        self.fullname = fullname
+        self.ainm = ainm
+        self.location = location
+
+    def __repr__(self):
+        return (
+            f"Club {self.id}: {self.name} (fullname={self.fullname}, ainm={self.ainm})"
+        )
+
+    def __str__(self):
+        return (
+            f"Club {self.id}: {self.name} (fullname={self.fullname}, ainm={self.ainm})"
+        )
+
+    def __eq__(self, other):
+        if not isinstance(other, Club):
+            return False
+        return (
+            self.name == other.name
+            and self.fullname == other.fullname
+            and self.ainm == other.ainm
+            and self.location == other.location
+        )
+
+    def update(self, name=None, fullname=None, ainm=None, location=None):
+        if name:
+            self.name = name
+        if fullname:
+            self.fullname = fullname
+        if ainm:
+            self.ainm = ainm
+        if location:
+            self.location = location
+
+
+class Referees(Base):
+    __tablename__ = "referees"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    club_name = mapped_column(String, ForeignKey("clubs.name"), nullable=False)
+    club = relationship("Club", backref="referees")
+
+    def __init__(self, name, club_name):
+        self.name = name
+        self.club_name = club_name
+
+    def __repr__(self):
+        return f"Referee {self.id}: {self.name} (club_name={self.club_name})"
+
+    def __str__(self):
+        return f"Referee {self.id}: {self.name} (club_name={self.club_name})"
+
+    def __eq__(self, other):
+        if not isinstance(other, Referees):
+            return False
+        return self.name == other.name and self.club_name == other.club_name
+
+    def update(self, name=None, club_name=None):
+        if name:
+            self.name = name
+        if club_name:
+            self.club_name = club_name
+
+
+class Venues(Base):
+    __tablename__ = "venues"
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    name: Mapped[str] = mapped_column(String, nullable=False)
+    club_name = mapped_column(String, ForeignKey("clubs.name"), nullable=False)
+    location = Column(Geometry("POINT"))
+
+
+class Grades(Base):
+    __tablename__ = "grades"
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    name: Mapped[str] = Column(String, nullable=False)
+    abbr: Mapped[str] = Column(String, nullable=False)
 
 
 class Player(Base):
     __tablename__ = "players"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)  # Name in English
-    ainm = Column(String, nullable=False)  # Ainm as Gaeilge
-    age = Column(
-        Integer, nullable=False
-    )  # Age in years on the qualifying date if <18, else 18
-    grade = Column(String, nullable=False)  # Junior, Intermediate, Senior
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    name: Mapped[str] = Column(String, nullable=False)  # Name in English
+    ainm: Mapped[str] = Column(String, nullable=False)  # Ainm as Gaeilge
     club_name = Column(String, ForeignKey("clubs.name"), nullable=False)
     club = relationship("Club", backref="players")
 
 
 class Competition(Base):
     __tablename__ = "competitions"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    name: Mapped[str] = Column(String, nullable=False)
     grade = Column(
         String, nullable=False
     )  # Under 14, Under 16, Minor, Junior, Intermediate, Senior
@@ -46,16 +124,16 @@ class Competition(Base):
 
 class Division(Base):
     __tablename__ = "divisions"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    name: Mapped[str] = Column(String, nullable=False)
     competition_id = Column(Integer, ForeignKey("competitions.id"), nullable=False)
     competition = relationship("Competition")
 
 
 class Team(Base):
     __tablename__ = "teams"
-    id = Column(Integer, primary_key=True)
-    name = Column(String, nullable=False)
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    name: Mapped[str] = Column(String, nullable=False)
     club_name = Column(String, ForeignKey("clubs.name"))
     division_id = Column(Integer, ForeignKey("divisions.id"))
     club = relationship("Club")
@@ -64,7 +142,8 @@ class Team(Base):
 
 class Match(Base):
     __tablename__ = "matches"
-    id = Column(Integer, primary_key=True)
+    id: Mapped[int] = Column(Integer, primary_key=True)
+    ref: Mapped[str] = Column(String, nullable=False)
     date = Column(Date, nullable=False)
     home_team_id = Column(Integer, ForeignKey("teams.id"), nullable=False)
     away_team_id = Column(Integer, ForeignKey("teams.id"), nullable=False)
@@ -89,12 +168,13 @@ player_competition_association = Table(
     Column("player_id", Integer, ForeignKey("players.id")),
     Column("competition_id", Integer, ForeignKey("competitions.id")),
     Column("team_id", Integer, ForeignKey("teams.id")),
+    Column("grade_id", Integer, ForeignKey("grade.id")),
 )
 
 
 class PlayerParticipation(Base):
     __tablename__ = "player_participation"
-    id = Column(Integer, primary_key=True)
+    id: Mapped[int] = Column(Integer, primary_key=True)
     match_id = Column(Integer, ForeignKey("matches.id"), nullable=False)
     player_id = Column(Integer, ForeignKey("players.id"), nullable=False)
     team_id = Column(Integer, ForeignKey("teams.id"), nullable=False)
